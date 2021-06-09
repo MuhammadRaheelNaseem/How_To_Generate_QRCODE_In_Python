9bbf28a07f565fe074cba652c13cb2d55cf42f8c
Muhammad Raheel <63813881+MuhammadRaheelNaseem@users.noreply.github.com>
2021-06-09 09:13:00 +0500

    Create qrcode.py

---
+++ qrcode.py
@@ -0,0 +1,15 @@
+# First install pyqrcode library using command line ya linux terminal
+
+import pyqrcode # Then allow library access
+import matplotlib.pyplot as plt
+import cv2
+
+# make variable to store link below there
+link = "www.facebook.com" 
+
+# make variable to create QR_CODE like this
+url=pyqrcode.create(link)
+
+# Then save it in any director Or folder
+img=url.png('QRCode.png',scale=6)
+plt.imshow(img)
